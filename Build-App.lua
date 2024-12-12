eee370fed7c545b7de35171e776cc2e8e4227de2
Sergei Kulenkov <serkulenkov@gmail.com>
2024-12-12 12:30:39 +0100

    fixed binaries folder

--- Build-App.lua
+++ Build-App.lua
@@ -12,8 +12,8 @@ project "Tree searching algorithms"
       "Source"
    }
 
-   targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
-   objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+   targetdir ("Binaries/" .. OutputDir .. "/%{prj.name}")
+   objdir ("Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
 
    filter "system:windows"
        systemversion "latest"
